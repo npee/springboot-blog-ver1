5f7b6befc7fbdd742deaddf433760146e88e20b2
MyeongchanJin <jmcchan@naver.com>
2020-05-08 18:10:37 +0900

    로그인 검증용 조건부 Select

--- src/main/java/com/npee/npeeblog/model/repository/UserJpaRepository.java
+++ src/main/java/com/npee/npeeblog/model/repository/UserJpaRepository.java
@@ -8,4 +8,5 @@ import java.util.Optional;
 public interface UserJpaRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByNickname(String nickname);
+    Optional<User> findByEmailAndPassword(String email, String password);
 }
